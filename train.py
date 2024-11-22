872023c3049864fddb5240c76fed2fb20b097b22
unknown <choiseungho1019@gmail.com>
2024-11-22 17:20:12 +0900

    add gcs

--- train.py
+++ train.py
@@ -7,7 +7,6 @@ import pyarrow.fs as pafs
 from datasets import Dataset
 from dataclasses import dataclass, field
 from typing import Optional
-import gcsfs
 
 from datasets import load_dataset, load_from_disk
 from transformers import (
@@ -65,8 +64,7 @@ def main():
     set_seed(training_args.seed)
 
     # Load dataset
-    storage_options={"project": "tribal-octane-442108-e7"}
-    gcs = gcsfs.GCSFileSystem(**storage_options)
+    gcs = pafs.GcsFileSystem()
     data_format = 'ipc'
 
     # PyArrow 데이터셋 생성 (디렉토리 내 모든 파일 로드)
