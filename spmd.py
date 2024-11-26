bd6489043be00976aace9ca1f9ed761d7ae69ab9
unknown <choiseungho1019@gmail.com>
2024-11-26 13:56:42 +0900

    fix error

--- spmd.py
+++ spmd.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python
-# coding=utf-8
-# Copyright 2020 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""
-Fine-tuning the library models for causal language modeling (GPT, GPT-2, CTRL, ...) on a text file or a dataset.
-
-Here is the full list of checkpoints on the hub that can be fine-tuned by this script:
-https://huggingface.co/models?filter=text-generation
-"""
-# You can also adapt this script on your own causal language modeling task. Pointers for this are left as comments.
-
 import logging
 import math
 import os
@@ -30,47 +7,25 @@ from dataclasses import dataclass, field
 from itertools import chain
 from typing import Optional
 import numpy as np
-import contextlib
-
-import datasets
-import evaluate
 import torch
-from datasets import load_dataset
 
 import transformers
 from transformers import (
-    CONFIG_MAPPING,
-    MODEL_FOR_CAUSAL_LM_MAPPING,
-    AutoConfig,
-    AutoModelForCausalLM,
     AutoTokenizer,
     HfArgumentParser,
     Trainer,
     TrainingArguments,
-    default_data_collator,
-    is_torch_xla_available,
     set_seed,
 )
-from transformers.testing_utils import CaptureLogger
-from transformers.trainer_utils import get_last_checkpoint
-from transformers.utils import check_min_version, send_example_telemetry
-from transformers.utils.versions import require_version
-from accelerate import init_empty_weights
-
-import torch_xla
-import torch_xla.debug.profiler as xp
-import torch_xla.runtime as xr
-import torch_xla.distributed.spmd as spmd
-
+import torch
+import torch_xla.distributed.xla_multiprocessing as xmp
+import torch_xla.core.xla_model as xm
+import torch_xla.distributed.spmd as xs
+import torch_xla.distributed.parallel_loader as pl
+import torch_xla.utils.utils as xu
+from torch_xla import runtime as xr
 # Enable SPMD mode execution
 xr.use_spmd()
-
-# Enable compile cache
-xr.initialize_cache("/tmp/files/tpu_cache", readonly=False)
-
-
-# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-
 
 logger = logging.getLogger(__name__)
 
@@ -99,15 +54,8 @@ class DataTrainingArguments:
     )
 @dataclass
 class TrainingArgumentsExtended(TrainingArguments):
-    spmd_2d_sharding: Optional[int] = field(
-        default=1,
-        metadata={"help": "The name of the dataset to use (via the datasets library)."},
-    )
-    spmd_dcn_parallelism: Optional[int] = field(
-        default=1,
-        metadata={"help": "The name of the dataset to use (via the datasets library)."},
-    )
-    
+    pass
+
 def main():
     # See all possible arguments in src/transformers/training_args.py
     # or by passing the --help flag to this script.
@@ -167,19 +115,6 @@ def main():
     # Set seed before initializing model.
     set_seed(training_args.seed)
 
-    server = xp.start_server(9012)
-    logger.info(f"Profiling server started: {str(server)}")
-
-    # Get the datasets: you can either provide your own CSV/JSON/TXT training and evaluation files (see below)
-    # or just provide the name of one of the public datasets available on the hub at https://huggingface.co/datasets/
-    # (the dataset will be downloaded automatically from the datasets Hub).
-    #
-    # For CSV/JSON files, this script will use the column called 'text' or the first column if no column called
-    # 'text' is found. You can easily tweak this behavior (see below).
-    #
-    # In distributed training, the load_dataset function guarantee that only one local process can concurrently
-    # download the dataset.
-    
     
     from transformers.models.electra import ElectraConfig
     from datasets import load_dataset, load_from_disk
@@ -212,111 +147,30 @@ def main():
     n_params = sum(p.numel() for p in disc.parameters())
     logger.info(f"Training new model from scratch - Total size={n_params / 2**20:.2f}M params")
 
-    
-    
 
-    import torch_xla.core.xla_model as xm
-    import torch_xla.distributed.spmd as xs
-
-
-    # Place DCN on an independent axis in the mesh. Model parameters should be
-    # replicated along the DCN axis, and inputs and activations should have
-    # the batch dimension sharded along the combined DCN and data axes.
     num_devices = xr.global_runtime_device_count()
-    model_axis = max(training_args.spmd_2d_sharding, 1)
-    assert (
-        xr.device_type() == "TPU" or xr.device_type() == "CUDA"
-    ), f"Supported hardware are TPU and CUDA. Detected hardware: {xr.device_type()}"
-    if xr.device_type() == "TPU":
-        dcn_axis = training_args.spmd_dcn_parallelism
-        data_axis = num_devices // model_axis // dcn_axis
-        ici_mesh_shape = (1, data_axis, model_axis)
-        dcn_mesh_shape = (dcn_axis, 1, 1)
-        spmd_mesh = xs.HybridMesh(
-            ici_mesh_shape=ici_mesh_shape,
-            dcn_mesh_shape=dcn_mesh_shape,
-            axis_names=("dcn", "data", "model"),
-        )
-        print("spmd_mesh:", spmd_mesh)
-        xs.set_global_mesh(spmd_mesh)
-    elif xr.device_type() == "CUDA":
-        data_axis = num_devices // model_axis
-        mesh_shape = (1, data_axis, model_axis)
-        spmd_mesh = xs.Mesh(
-            np.arange(num_devices), mesh_shape, ("dcn", "data", "model")
-        )
-
-    # Update training args with relevant SPMD config
-    training_args.spmd_mesh = spmd_mesh
-    # Replace the linear layer
-    from torch_xla.distributed.fsdp.utils import apply_xla_patch_to_nn_linear
-
-    model = apply_xla_patch_to_nn_linear(model, xs.xla_patched_nn_linear_forward)
-
-    model = model.to(xm.xla_device(), dtype=getattr(torch, model_args.torch_dtype))
-
-    # Shard each parameter in the model based on the sharding strategy provided.
-    for name, param in model.named_parameters():
-        if training_args.spmd_2d_sharding > 0:
-            # Apply 2D sharding:
-            print("> [2D] Sharding tensor", name, param.shape)
-
-            # We don't care about layernorm's weights, and
-            # LLaMA doesn't use biases.
-            if len(param.shape) == 1:
-                continue
-            
-            if "embeddings" in name or "word_embeddings" in name:
-                xs.mark_sharding(param, spmd_mesh, ("model", "data"))
-
-            # Self-Attention layers
-            elif "attention.self.query" in name or "attention.self.key" in name or "attention.self.value" in name:
-                xs.mark_sharding(param, spmd_mesh, ("data", "model"))
-            elif "attention.self.output.dense" in name:
-                xs.mark_sharding(param, spmd_mesh, ("model", "data"))
-
-            # Feed-Forward Network (FFN)
-            elif "intermediate.dense" in name:  # FFN's intermediate layer
-                xs.mark_sharding(param, spmd_mesh, ("model", "data"))
-            elif "output.dense" in name:  # FFN's output layer
-                xs.mark_sharding(param, spmd_mesh, ("data", "model"))
-
-            # Discriminator predictions
-            elif "discriminator_predictions.dense" in name or "dense_prediction" in name or "cls_representation" in name:
-                xs.mark_sharding(param, spmd_mesh, ("model", "data"))
-
-            # Generator predictions
-            elif "generator_predictions.dense" in name or "lm_head" in name or "generator_score_head" in name:
-                xs.mark_sharding(param, spmd_mesh, ("model", "data"))
-
-        print(f"{name} {torch_xla._XLAC._get_xla_sharding_spec(param)}")
+    device_ids = np.arange(num_devices)
+    mesh_shape = (num_devices,)
+    mesh = xs.Mesh(device_ids, mesh_shape, ('data',))
+    batch_size = num_devices * training_args.per_device_train_batch_size
+    
+    print(mesh, batch_size)
 
 
-    for i, layer in enumerate(model.electra.encoder.layer):
-        spmd.xla_sharding.apply_backward_optimization_barrier(layer)
-    for i, layer in enumerate(model.generator.electra.encoder.layer):
-        spmd.xla_sharding.apply_backward_optimization_barrier(layer)
+    # trainer = Trainer(
+    #     model=model,
+    #     args=training_args,
+    #     train_dataset=datasets,
+    #     tokenizer=tokenizer,
+    # )
 
-    # for i, block in enumerate(model.model.layers):
-    #     # LLaMA-specific
-    #     # xs.apply_backward_optimization_barrier(model.model.layers[i])
-    #     spmd.xla_sharding.apply_backward_optimization_barrier(model.model.layers[i])
+    # # Test model saving
+    # # if training_args.do_train:
+    # #     print("Saving model before training")
+    # #     trainer.save_model()
+    # #     print("Model saved")
 
-    # Initialize our Trainer
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=datasets,
-        tokenizer=tokenizer,
-    )
-
-    # Test model saving
-    # if training_args.do_train:
-    #     print("Saving model before training")
-    #     trainer.save_model()
-    #     print("Model saved")
-
-    trainer.train()
+    # trainer.train()
 
 
 
