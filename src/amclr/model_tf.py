e0e8d186eb978889d8e75b9e4da538b440217272
unknown <choiseungho1019@gmail.com>
2024-12-03 13:56:44 +0900

    c

--- src/amclr/model_tf.py
+++ src/amclr/model_tf.py
@@ -231,7 +231,7 @@ class AMCLR_TF(TFElectraForPreTraining):
             shape=[self.config.vocab_size]
         )
         mask_special_tokens_expanded = tf.reshape(mask_special_tokens, [1, 1, -1])
-        mask_input_ids = tf.one_hot(input_ids.input_ids, depth=self.config.vocab_size, dtype=tf.bool)
+        mask_input_ids = tf.one_hot(input_ids, depth=self.config.vocab_size, dtype=tf.bool)
 
         total_mask = mask_special_tokens_expanded | mask_input_ids
 
