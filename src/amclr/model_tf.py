756e61d82d1c05b9204cf444f807ae3c194a39fc
unknown <choiseungho1019@gmail.com>
2024-12-03 14:07:12 +0900

    c

--- src/amclr/model_tf.py
+++ src/amclr/model_tf.py
@@ -124,6 +124,30 @@ class AMCLRProject(TFElectraGeneratorPredictions):
                 # Build the dense layer with appropriate input shape
                 self.dense.build([None, None, self.config.hidden_size])
 
+@dataclass
+class Myoutput(ModelOutput):
+    """
+    Base class for masked language models outputs.
+
+    Args:
+        loss (`tf.Tensor` of shape `(n,)`, *optional*, where n is the number of non-masked labels, returned when `labels` is provided):
+            Masked language modeling (MLM) loss.
+        logits (`tf.Tensor` of shape `(batch_size, sequence_length, config.vocab_size)`):
+            Prediction scores of the language modeling head (scores for each vocabulary token before SoftMax).
+        hidden_states (`tuple(tf.Tensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `tf.Tensor` (one for the output of the embeddings + one for the output of each layer) of shape
+            `(batch_size, sequence_length, hidden_size)`.
+
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (`tuple(tf.Tensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+            Tuple of `tf.Tensor` (one for each layer) of shape `(batch_size, num_heads, sequence_length,
+            sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    loss: tf.Tensor | None = None
 
 class AMCLR_TF(TFElectraForPreTraining):
     def __init__(self, config, special_token_ids, **kwargs):
@@ -346,4 +370,4 @@ class AMCLR_TF(TFElectraForPreTraining):
         
         loss = disc_loss * self.l1 + sims_loss * self.l2
         
-        return ((loss,))
+        return Myoutput(loss=loss)
