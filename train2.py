3d1a4872fb61f480042e091a35924b8422309388
unknown <choiseungho1019@gmail.com>
2025-01-01 23:32:21 +0900

    temp

--- train2.py
+++ train2.py
@@ -181,20 +181,20 @@ def main(rank):
                 disc = unwrapped_model.electra
                 save_path = os.path.join(training_args.output_dir, f"disc-checkpoint-{global_step}")
                 xm.master_print(f"Saving model checkpoint to {save_path}")
-                disc.save_pretrained(
-                save_path,
-                is_main_process=xm.is_master_ordinal(local=False),
-                state_dict=xm._maybe_convert_to_cpu(disc.state_dict()),
-                save_function=xm.save,
-                )
-                save_path = os.path.join(training_args.output_dir, f"gen-checkpoint-{global_step}")
-                xm.master_print(f"Saving model checkpoint to {save_path}")
-                gen.save_pretrained(
-                save_path,
-                is_main_process=xm.is_master_ordinal(local=False),
-                state_dict=xm._maybe_convert_to_cpu(gen.state_dict()),
-                save_function=xm.save,
-                )
+                # disc.save_pretrained(
+                # save_path,
+                # is_main_process=xm.is_master_ordinal(local=False),
+                # state_dict=xm._maybe_convert_to_cpu(disc.state_dict()),
+                # save_function=xm.save,
+                # )
+                # save_path = os.path.join(training_args.output_dir, f"gen-checkpoint-{global_step}")
+                # xm.master_print(f"Saving model checkpoint to {save_path}")
+                # gen.save_pretrained(
+                # save_path,
+                # is_main_process=xm.is_master_ordinal(local=False),
+                # state_dict=xm._maybe_convert_to_cpu(gen.state_dict()),
+                # save_function=xm.save,
+                # )
 
             # 특정 스텝마다 wandb에 로깅
             if global_step % training_args.logging_steps == 0:
